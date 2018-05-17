55e1e7987cafe73c1fd10fca268b16d22b744c03
Unknown <max_van_kessel@msn.com>
2018-05-17 22:31:02 +0200

    net/hdlc: extended hdlc hdr content

--- sys/include/net/hdlc/hdr.h
+++ sys/include/net/hdlc/hdr.h
@@ -28,6 +28,49 @@
 extern "C" {
 #endif
 
+typedef enum {
+    HDLC_TYPE_RECEIVE_READY     = 0,
+    HDLC_TYPE_RECEIVE_NOT_READY = (1 << 1),
+    HDLC_TYPE_REJECT            = (1 << 0),
+    HDLC_TYPE_SELECTIVE_REJECT  = (3 << 0),
+} hdlc_type_t;
+
+typedef enum {
+    HDLC_FRAME_TYPE_INFORMATION = 0,
+    HDLC_FRAME_TYPE_SUPERVISORY = 1,
+    HDLC_FRAME_TYPE_UNNUMBERED = 3,
+} hdlc_frame_type_t;
+
+/**
+ * @brief HDLC Supervisory Frames
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t id : 2;
+    hdlc_type_t type : 2;
+    uint8_t poll_final : 1;
+    uint8_t sequence_no : 3;
+} hdlc_control_s_frame_t;
+
+/**
+ * @brief HDLC Information Frames
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t id : 1;
+    uint8_t send_sequence_no : 3;
+    uint8_t poll_final : 1;
+    uint8_t sequence_no : 3;
+} hdlc_control_i_frame_t;
+
+/**
+ * @brief HDLC Unnumbered Frames
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t id : 2;
+    hdlc_type_t type : 2;
+    uint8_t poll_final : 1;
+    hdlc_type_t type_x : 3;
+} hdlc_control_u_frame_t;
+
 /**
  * @brief Data type to represent an HDLC header.
  *
@@ -46,8 +89,14 @@ extern "C" {
  *
  */
 typedef struct __attribute__((packed)) {
-    uint8_t address;            /**< Source address field of HDLC header */
-    uint8_t control;            /**< Control field of HDLC header */
+    uint8_t address;            /**< Address field oh HDLC header */
+    union{
+        hdlc_control_s_frame_t s;            /**< Control field of HDLC header */
+        hdlc_control_i_frame_t i;
+        hdlc_control_u_frame_t u;
+        uint8_t frame;
+    }control;
+    network_uint16_t protocol;  /**< Protocol field of HDLC header */
 } hdlc_hdr_t;
 
 #ifdef __cplusplus
