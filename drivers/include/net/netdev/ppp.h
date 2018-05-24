6ecae4318796a3008d3f267774198f405561b73e
Unknown <max_van_kessel@msn.com>
2018-05-24 02:26:19 +0200

    net/gnrc/ppp: corrected netdev header file

--- drivers/include/net/netdev/ppp.h
+++ drivers/include/net/netdev/ppp.h
@@ -19,35 +19,32 @@
 #ifndef NETDEV_PPP_H
 #define NETDEV_PPP_H
 
-//#include "net/gnrc/ppp/prot.h"
-//
-//#include "net/gnrc/ppp/lcp.h"
-//#include "net/gnrc/ppp/pap.h"
-//#include "net/gnrc/ppp/ipcp.h"
-
 #include "net/gnrc/nettype.h"
 #include "net/netopt.h"
 #include "net/netdev.h"
+
+#include "xtimer.h"
+#include "net/gnrc/ppp/prot.h"
+#include "net/gnrc/ppp/lcp.h"
+#include "net/gnrc/ppp/pap.h"
+#include "net/gnrc/ppp/ipcp.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if defined(MODULE_GNRC_PPP) || DOXYGEN
 /**
  * @brief class of custom driver control protocol
  * @extends ppp_protocol_t
  *
  * @details the DCP is in charge of monitoring the link and exchanging messages with the ppp device
  */
-//typedef struct gnrc_ppp_dcp {
-//    gnrc_ppp_protocol_t prot;   /**< base ppp_protocol_t object */
-//    msg_t timer_msg;            /**< msg struct for handling timeouts messages */
-//    xtimer_t xtimer;            /**< xtimer struct for sending timeout messages */
-//    uint8_t dead_counter;       /**< when reaches zero, the link is assumed to be dead */
-//} gnrc_ppp_dcp_t;
-#endif
-
+typedef struct gnrc_ppp_dcp {
+    gnrc_ppp_protocol_t prot;    /**< base ppp_protocol_t object */
+    msg_t timer_msg;        /**< msg struct for handling timeouts messages */
+    xtimer_t xtimer;        /**< xtimer struct for sending timeout messages */
+    uint8_t dead_counter;   /**< when reaches zero, the link is assumed to be dead */
+} gnrc_ppp_dcp_t;
 
 /**
  * @brief Extended structure to hold PPP driver state
@@ -59,19 +56,14 @@ extern "C" {
  */
 typedef struct netdev_ppp {
     netdev_t netdev;                       /**< @ref netdev_t base class */
-#if defined(MODULE_GNRC_PPP) || DOXYGEN
-    /**
-     * @brief PPP specific fields
-     * @{
-     */
-//    gnrc_ppp_dcp_t dcp;                    /**< Control protocol for driver */
-//    gnrc_ppp_lcp_t lcp;                    /**< Link Control Protocol */
-//    gnrc_ppp_pap_t pap;                    /**< Password Authentication Protocol */
-//    gnrc_ppp_ipcp_t ipcp;                  /**< IPv4 Network Control Protocol */
-//    gnrc_ppp_ipv4_t ipv4;                  /**< Handler for IPv4 packets */
-#endif
-} netdev_ppp_t;
 
+    gnrc_ppp_dcp_t dcp;                             /**< Control protocol for driver */
+    gnrc_ppp_lcp_t lcp;                             /**< Link Control Protocol */
+    gnrc_ppp_pap_t pap;                             /**< Password Authentication Protocol */
+    gnrc_ppp_ipcp_t ipcp;                  /**< IPv4 Network Control Protocol */
+    gnrc_ppp_ipv4_t ipv4;                  /**< Handler for IPv4 packets */
+
+} netdev_ppp_t;
 
 /**
  * @brief   Fallback function for netdev PPP devices' _get function
@@ -86,8 +78,7 @@ typedef struct netdev_ppp {
  * @return              number of bytes written to @p value
  * @return              <0 on error
  */
-int netdev_ppp_get(netdev_ppp_t *dev, netopt_t opt, void *value,
-                    size_t max_len);
+int netdev_ppp_get(netdev_ppp_t *dev, netopt_t opt, void *value, size_t max_len);
 
 /**
  * @brief   Fallback function for netdev PPP devices' _set function
@@ -101,8 +92,7 @@ int netdev_ppp_get(netdev_ppp_t *dev, netopt_t opt, void *value,
  * @return              number of bytes used from @p value
  * @return              <0 on error
  */
-int netdev_ppp_set(netdev_ppp_t *dev, netopt_t opt, const void *value,
-                    size_t value_len);
+int netdev_ppp_set(netdev_ppp_t *dev, netopt_t opt, const void *value, size_t value_len);
 
 #ifdef __cplusplus
 }
